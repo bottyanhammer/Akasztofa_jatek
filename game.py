f9344d703995635233925a74199fdf2b36df238d
bottyan.hammer <bottyan.hammer@gmail.com>
2023-12-19 14:19:50 +0100

    Karakter ellenőrzése

--- game.py
+++ game.py
@@ -1,3 +1,27 @@
 import random
 
-wordsList = ["kelkáposzta","cvekedli","sztrapacska","vánkos","kamra"]
+def randomWordGenerate() -> str:
+    wordsList = ["alma", "körte", "barack", "szilva", "narancs", "eper", "citrom", "mandula", "datolya", "eperfa"]
+    return random.choice(wordsList)
+
+wordTemplate = list()
+word = randomWordGenerate()
+print(word)
+#List-comprehension ehelyett jobb lesz.
+for i in range(len(word)):
+    wordTemplate.append("_")
+
+print()
+print("  ".join(wordTemplate))
+
+print("\nTaláld ki a gondolt szót! 10 lehetőséged van, hogy megadj egy-egy karaktert.")
+tip = input("Az ön tippje: ")
+
+if tip not in word:
+    print("Hát ez bizony rossz ötlet volt.")
+else:
+    for i in range(len(word)):
+        if word[i] == tip:
+            wordTemplate[i] = tip
+
+print("  ".join(wordTemplate))
